2ce20f4f25d0d793404f2e9c31b45f31af010f51
sebastosh <accounts@sebastiensanzdesantamaria.net>
2025-12-11 12:03:30 -0600

    tweaks via WSL

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
 // import { useState } from 'react'
+import metaData from "./data/meta.json";
 import "./assets/style.css";
 import Choices from "./components/choices";
 import Exhibition from "./components/exhibition";
 import Signatories from "./components/signatories";
 
 function App() {
+
+  const meta = metaData;
 
 
   const handlePrint = () => {
@@ -13,24 +16,10 @@ function App() {
 
   
   return (
-    <>
+    <div id="main">
       <header>
-        <h1>
-          <strong>MATTER MATTERS:</strong> RIDER FOR EXHIBITION AGREEMENTS
-        </h1>
-        <p className="description">
-          This is an exhibition agreement supplement for those of us who reckon
-          with art and want coherence between our concern - regarding climate
-          disruption and the megaflop of plastics-reliance and how we live among
-          other sentient beings whose lives we do not think are less worthy of
-          attention than our own - and how we actually go about things. They are
-          mores, not lesses; while they are limitations, they are not
-          restrictive. In fact,we consider them beautiful — a hedonic ethos. We
-          aim to commit to them in a spirit of friendly disagreeableness: a
-          friendly disagreeableness with the general customs and assumptions,
-          within the mainstream of cultural production, of what is necessary to
-          make/meet art today.
-        </p>
+        <h1><strong>{meta.title}:</strong> {meta.subtitle}</h1>
+        <p className="description">{meta.description}</p>
       </header>
 
       <form>
@@ -40,7 +29,7 @@ function App() {
         <Signatories />
       </form>
        <button className="print" onClick={handlePrint}>Print</button>
-    </>
+    </div>
   );
 }
 
--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 import netlify from "@netlify/vite-plugin";
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react(),netlify()],
 })
